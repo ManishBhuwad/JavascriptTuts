4f80e68b1cef42ed292002c3f23fb128337c1186
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2022-12-26 14:17:19 +0000

    learn to declare variables

---
+++ 01_Basics/01variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+let accountEmail = "manish@gmail.com";
+var accountPassword = "123483";
+accountCity = "Dapoli"; // you can assing memory to the variable without declaring the type of the variable
+
+
+/* 
+Prefer not used var
+Because the issue in block scope and functional scope;
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);

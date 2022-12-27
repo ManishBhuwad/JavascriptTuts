c9477615dd24c8113f289b425fd409b5200a4b9f
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2022-12-27 15:39:48 +0000

    datatypes

---
+++ 01_Basics/02datatypes.js
@@ -0,0 +1,12 @@
+let string = "this is string";
+let number = 33;
+let accountIsLoggedIn = false; // this is boolean data type; this has either true or false value;
+let name;    //-----> this is undefined as the variable is declared but not assigned any value;
+sirname= null; //-----> there is null datatype which means null that does not hold any value;
+
+/*
+
+object ----> it is also a type of datatype
+
+
+*/

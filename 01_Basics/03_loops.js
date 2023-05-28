bb1f89ff0ead6f98f02fbbbcd9996f5d0d0f377b
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2023-05-28 05:38:19 +0000

    loops and object

---
+++ 01_Basics/03_loops.js
@@ -0,0 +1,27 @@
+// while loop
+
+let score = 10;
+let i = 0;
+
+while(i<=score){
+    console.log(`value is : ${i}`);
+    i++;
+}
+
+const myArray = ['manish', 'amey', 'akshay', 'raj'];
+let arr = 0;
+
+while(arr<myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+}
+
+
+// DO while loop 
+
+let num = 10
+let j = 0
+do{
+    console.log(num);
+    j++
+}while(j<num);
---
+++ 04_Basics/01_Object.js
@@ -0,0 +1,16 @@
+
+const myObject = {
+    // name: "manish",
+    // sirname: "Bhuwad",
+    // middleName: "Dilip"
+}
+
+// console.log(myObject.sirname);
+
+// how to check the object is empty or not
+
+if(Object.keys(myObject).length===0){
+    console.log("object is empty");
+}else{
+    console.log('object is not empty');
+}
